ec8d7cc69405bf162efaa7dd71a35581a18dd2e0
skuhlmann <samkuhlmann@gmail.com>
2022-03-10 16:51:19 -0700

    test refactor

--- libs/haus-sdk/src/lib/haus-sdk.spec.ts
+++ libs/haus-sdk/src/lib/haus-sdk.spec.ts
@@ -1,26 +1,24 @@
 import Haus from './haus-sdk';
 
 describe('hausSdk', () => {
-  it('can init with network params', async () => {
-    const params = {
-      provider: 'yolo',
-      graphUrl:
-        'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-rinkeby',
-    };
-    const haus = await Haus.create(params);
+  const params = {
+    provider: 'yolo',
+    graphUrl:
+      'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-rinkeby',
+  };
+  let haus: Haus;
 
+  beforeAll(async () => {
+    haus = await Haus.create(params);
+  });
+
+  it('can init with network params', async () => {
     expect(haus.provider).toEqual(params.provider);
     expect(haus.graphUrl).toEqual(params.graphUrl);
   });
 
   it('returns dao address', async () => {
-    const params = {
-      provider: 'yolo',
-      graphUrl:
-        'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-rinkeby',
-    };
     const daoAddress = 'poopin';
-    const haus = await Haus.create(params);
     const res = await haus.getDao(daoAddress);
 
     expect(res).toEqual(daoAddress);
