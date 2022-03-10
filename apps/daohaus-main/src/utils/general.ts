0f61d86e1f50c9894bc1a561fb19a751268da621
jordan <jordan.lesich@gmail.com>
2022-03-10 10:43:45 -0800

    typecheck utils, copy over chain/other crap from old app

--- apps/daohaus-main/src/utils/general.ts
+++ apps/daohaus-main/src/utils/general.ts
@@ -6,3 +6,16 @@ export const getNonce = (length = 16) => {
   }
   return text;
 };
+
+export const isBoolean = (item: unknown) => typeof item === 'boolean';
+export const isString = (item: unknown) => typeof item === 'string';
+export const isNumber = (item: unknown) => typeof item === 'number';
+export const isArray = (item: unknown) => Array.isArray(item);
+export const isObject = (item: unknown) =>
+  item &&
+  typeof item === 'object' &&
+  !Array.isArray(item) &&
+  typeof item !== 'function';
+
+export const isErrorType = (item: unknown) =>
+  item && typeof item === 'object' && 'error' in item && 'message' in item;
