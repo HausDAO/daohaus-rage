77050f69f8e2182eb8ea39ab57ab7960fd120df2
jordan <jordan.lesich@gmail.com>
2022-03-10 10:44:57 -0800

    clean

--- apps/daohaus-main/src/main.tsx
+++ apps/daohaus-main/src/main.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { WalletProvider } from '@raidguild/quiver';
 import App from './app/app';
-import { initializationActions } from './utils/summon';
+
 import { SUPPORTED_NETWORKS } from './utils/wallet';
 
 ReactDOM.render(
