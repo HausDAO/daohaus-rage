3a41c1e69e27e19552c287135ef0367f12815605
skuhlmann <samkuhlmann@gmail.com>
2022-03-10 16:44:00 -0700

    some tests and playing with init

--- libs/haus-sdk/src/lib/haus-sdk.spec.ts
+++ libs/haus-sdk/src/lib/haus-sdk.spec.ts
@@ -1,7 +1,28 @@
-import { hausSdk } from './haus-sdk';
+import Haus from './haus-sdk';
 
 describe('hausSdk', () => {
-  it('should work', () => {
-    expect(hausSdk()).toEqual('poopin');
+  it('can init with network params', async () => {
+    const params = {
+      provider: 'yolo',
+      graphUrl:
+        'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-rinkeby',
+    };
+    const haus = await Haus.create(params);
+
+    expect(haus.provider).toEqual(params.provider);
+    expect(haus.graphUrl).toEqual(params.graphUrl);
+  });
+
+  it('returns dao address', async () => {
+    const params = {
+      provider: 'yolo',
+      graphUrl:
+        'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-rinkeby',
+    };
+    const daoAddress = 'poopin';
+    const haus = await Haus.create(params);
+    const res = await haus.getDao(daoAddress);
+
+    expect(res).toEqual(daoAddress);
   });
 });
--- libs/haus-sdk/src/lib/haus-sdk.ts
+++ libs/haus-sdk/src/lib/haus-sdk.ts
@@ -1,3 +1,38 @@
 export function hausSdk(): string {
   return 'poopin';
 }
+
+// interface NetworkParams {
+//   provider: string;
+//   graphEndpoint: string;
+// }
+
+interface HausConfig {
+  provider: string;
+  graphUrl: string;
+}
+
+class Haus {
+  provider!: string;
+  graphUrl!: string;
+
+  static async create({ provider, graphUrl }: HausConfig): Promise<Haus> {
+    const hausSdk = new Haus();
+    await hausSdk.init({
+      provider,
+      graphUrl,
+    });
+    return hausSdk;
+  }
+
+  private async init({ provider, graphUrl }: HausConfig): Promise<void> {
+    this.provider = provider;
+    this.graphUrl = graphUrl;
+  }
+
+  async getDao(daoAddress: string): Promise<string> {
+    return daoAddress;
+  }
+}
+
+export default Haus;
