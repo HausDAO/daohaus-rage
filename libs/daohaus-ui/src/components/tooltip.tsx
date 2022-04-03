e69eaf1d8e1dbb76256392d548ae4ba92111be2e
jordan <jordan.lesich@gmail.com>
2022-04-03 14:40:58 -0700

    refactor animations

--- libs/daohaus-ui/src/components/tooltip.tsx
+++ libs/daohaus-ui/src/components/tooltip.tsx
@@ -1,88 +1,26 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-import { PlusIcon } from '@radix-ui/react-icons';
-import { violet, blackA } from '@radix-ui/colors';
+import styled from 'styled-components';
+
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import { BORDER, COLOR, FONT } from '../styles/global';
 import { BiErrorCircle } from 'react-icons/bi';
-import { JsxElement } from 'typescript';
-import { IconType } from 'react-icons/lib';
-
-const fadeIn = keyframes`
-  from{
-    opacity: 0
-  }
-  to {
-    opactity: 1
-  }
-`;
-const fadeOut = keyframes`
-from{
-    opacity: 1
-  }
-  to {
-    opactity: 0
-  }
-`;
-
-// const slideUpAndFade = keyframes({
-//   '0%': { opacity: 0, transform: 'translateY(2px)' },
-//   '100%': { opacity: 1, transform: 'translateY(0)' },
-// });
-
-// const slideRightAndFade = keyframes({
-//   '0%': { opacity: 0, transform: 'translateX(-2px)' },
-//   '100%': { opacity: 1, transform: 'translateX(0)' },
-// });
-
-// const slideDownAndFade = keyframes({
-//   '0%': { opacity: 0, transform: 'translateY(-2px)' },
-//   '100%': { opacity: 1, transform: 'translateY(0)' },
-// });
-
-// const slideLeftAndFade = keyframes({
-//   '0%': { opacity: 0, transform: 'translateX(2px)' },
-//   '100%': { opacity: 1, transform: 'translateX(0)' },
-// });
+import { fadeIn } from '../utils/animations';
 
 const StyledContent = styled(TooltipPrimitive.Content)`
-  /* background-color: '${COLOR.BG[100]}'; */
   background-color: ${COLOR.BG[100]};
   border-radius: ${BORDER.RADIUS};
   padding: 1.2rem 1.5rem;
   font-size: ${FONT.SIZE.SM};
   line-height: 2.4rem;
   color: ${FONT.COLOR};
+  &[data-state='delayed-open'] {
+    animation: ${fadeIn} 0.2s ease-in forwards;
+  }
 `;
-
-// const StyledContent = styled(TooltipPrimitive.Content, {
-//   borderRadius: 4,
-//   padding: '10px 15px',
-//   fontSize: 15,
-//   lineHeight: 1,
-//   color: violet.violet11,
-//   backgroundColor: 'white',
-//   boxShadow:
-//     'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
-//   '@media (prefers-reduced-motion: no-preference)': {
-//     animationDuration: '400ms',
-//     animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
-//     animationFillMode: 'forwards',
-//     willChange: 'transform, opacity',
-//     '&[data-state="delayed-open"]': {
-//       '&[data-side="top"]': { animationName: slideDownAndFade },
-//       '&[data-side="right"]': { animationName: slideLeftAndFade },
-//       '&[data-side="bottom"]': { animationName: slideUpAndFade },
-//       '&[data-side="left"]': { animationName: slideRightAndFade },
-//     },
-//   },
-// });
 
 const StyledArrow = styled(TooltipPrimitive.Arrow)`
   fill: ${COLOR.BG[100]};
 `;
-
-// Exports
 
 export const TooltipRoot = TooltipPrimitive.Root;
 export const TooltipTrigger = styled(TooltipPrimitive.Trigger)`
@@ -90,23 +28,6 @@ export const TooltipTrigger = styled(TooltipPrimitive.Trigger)`
   border: none;
 `;
 export const TooltipContent = StyledContent;
-
-// Your app...
-// const IconButton = styled('button', {
-//   all: 'unset',
-//   fontFamily: 'inherit',
-//   borderRadius: '100%',
-//   height: 35,
-//   width: 35,
-//   display: 'inline-flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-//   color: violet.violet11,
-//   backgroundColor: 'white',
-//   boxShadow: `0 2px 10px ${blackA.blackA7}`,
-//   '&:hover': { backgroundColor: violet.violet3 },
-//   '&:focus': { boxShadow: `0 0 0 2px black` },
-// });
 
 const Tooltip = ({
   content = 'Your content here',
