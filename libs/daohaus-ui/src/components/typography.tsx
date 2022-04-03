10daaa61df6add31793d0125c39df7a023131afa
jordan <jordan.lesich@gmail.com>
2022-04-02 21:06:00 -0700

    refactor style system

---
+++ libs/daohaus-ui/src/components/typography.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const P = styled.p`
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+  letter-spacing: 0.8px;
+  color: white;
+  margin-bottom: 1.1rem;
+`;
---
+++ libs/daohaus-ui/src/styles/global.ts
@@ -0,0 +1,27 @@
+export const COLOR = {
+  BG: {
+    100: 'rgba(255, 255, 255, 0.1)',
+    200: 'rgba(255, 255, 255, 0.2)',
+    500: 'rgba(255, 255, 255, 0.5)',
+  },
+
+  STANDARD: '#FFFFFF',
+  PLACE_HOLDER: 'rgba(255, 255, 255, 0.5)',
+};
+// common styles across all borders
+
+export const BORDER = {
+  RADIUS: '0.4rem',
+};
+
+//  common styles across all fonts
+export const FONT = {
+  SIZE: {
+    SM: '1.6rem',
+  },
+  FAMILY: {
+    BODY: 'Mulish',
+  },
+  COLOR: COLOR.STANDARD,
+  COLOR_PLACE_HOLDER: COLOR.BG[500],
+};
